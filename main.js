2ece7b2404f52a0148f6e5bf6d6a57ad89cdf6cf
AUD-GITHUB <85228103+AUD-GITHUB@users.noreply.github.com>
2023-05-23 15:00:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+}
+);
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot(){
+    Webcam.snap( function(data_uri) {
+        document.getElementById("result").innerHTML = 
+         '<img src="'+data_uri+'"/>';
+    } );
+    Completed 
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Bes2gY9b-/model.json', ModelLoaded);
+
+function ModelLoaded() {
+    console.log('Model is Loaded');
+}
